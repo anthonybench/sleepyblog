4583c7e035926e68c74009c2a3812ef07f23ddcb
anthonybench <anthonybenchyep@gmail.com>
2024-03-20 00:52:34 -0700

    removed dead code

--- app/_lib/fonts.ts
+++ app/_lib/fonts.ts
@@ -1,12 +1,9 @@
-import { MoreHorizontal } from "lucide-react";
 import {
   Inter,
   Lusitana,
   JetBrains_Mono,
   Fira_Code,
   Roboto_Mono,
-  Single_Day,
-  Moirai_One,
   Comfortaa,
   Playball,
   Sacramento,
@@ -52,15 +49,6 @@ export const cookie = Cookie({
   subsets: ["latin"],
   weight: ["400"],
 });
-// Sixtyfour :: how do i install it?
-// export const single_day = Single_Day({
-//   subsets: ['latin'],
-//   weight: ['400', '700'],
-// });
-// export const moirai_one = Moirai_One({
-//   subsets: ['latin'],
-//   weight: ['400', '700'],
-// });
 export const comfortaa = Comfortaa({
   subsets: ["latin"],
   weight: ["400", "700"],
