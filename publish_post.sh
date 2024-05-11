a02b5ea68e325d961ce5252da2fb4b4d7b844a38
anthonybench <anthonybenchyep@gmail.com>
2024-05-10 17:09:22 -0700

    more automation scripting for common workflows

---
+++ publish_post.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# ./publish_post.sh
+
+git add public/assets/posts _posts/;
+git commit -m "BLOG POST :: '$(date)'";
+git push origin main --force;
--- stub_post.sh
+++ stub_post.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
+# ./stub_post.sh
 
-POST_DATE=2024-04-03;
-
+POST_DATE=2024-05-10;
 
 mkdir public/assets/posts/$POST_DATE;
 cp _templates/_posts/yyyy-mm-dd.md _posts/${POST_DATE}.md
