816655991b80e77efd9546566f14d3033f47ff7e
anthonybench <anthonybenchyep@gmail.com>
2024-10-03 14:22:34 -0700

    ugh

--- app/_lib/utils.ts
+++ app/_lib/utils.ts
@@ -23,6 +23,7 @@ const slugs = [
   "2024-07-24",
   "2024-08-11",
   "2024-09-03",
+  "2024-09-24",
 ];
 
 export function cn(...inputs: ClassValue[]) {
--- app/last-updated-date.ts
+++ app/last-updated-date.ts
@@ -1,2 +1,2 @@
 // yyyy-mm-dd
-export const lastUpdatedDate: string = "2024-09-25";
+export const lastUpdatedDate: string = "2024-10-03";
